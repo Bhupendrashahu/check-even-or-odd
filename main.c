fab57cd83bbc99a2f61c3e05f3fc0a61ace4f2f7
Bhupendrashahu <76242175+Bhupendrashahu@users.noreply.github.com>
2021-04-08 10:38:28 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+int n;
+printf("Enter the no for check the even or odd \n");
+scanf("%d",&n);
+if (n%2==0)
+printf("NUMBER IS EVEN  \n ",n);
+else
+    printf("NUMBER IS ODD
+            \n",n);
+    return 0;
+}
